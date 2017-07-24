40a1fbe0efb0386d90de0afc615e28cd072f32f1
John Fish <j31@users.noreply.github.com>
2017-07-24 16:19:36 +0200

    bootstrap template

--- app.rb
+++ app.rb
@@ -2,11 +2,23 @@ require "sinatra"
 require "sinatra/reloader" if development?
 # require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  '<h1>Sinatra is cool!</h1>'
+  @usernames = ['tom', 'dick', 'harry']
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
